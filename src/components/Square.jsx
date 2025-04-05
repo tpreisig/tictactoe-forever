3c22878d582e2a8ebd5d0e053c86be52e33d4953
Tiziano Preisig <tiziano.preisig@hispeed.ch>
2025-04-05 11:07:09 +0200

    added structure

---
+++ src/components/Square.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <>
+      <div className='board-row'>
+        <div>Square1</div>
+        <div>Square2</div>
+        <div>Square3</div>
+      </div>
+      <div className='board-row'>
+        <div>Square4</div>
+        <div>Square5</div>
+        <div>Square6</div>
+      </div>
+      <div className='board-row'>
+        <div>Square7</div>
+        <div>Square8</div>
+        <div>Square9</div>
+      </div>
+   
+     
+    </>
+  )
+}
+
+export default Square
